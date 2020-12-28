d8001c6f13c75942df58cb2ec5a9b369e7f29826
Artiom Kalugin <a.o.kalugin@protonmail.com>
2020-12-28 08:59:43 +0000

    Added more structure and dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-const App = () => <h1>Hello World!</h1>;
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/signup">
+                    <h1>Sign Up</h1>
+                </Route>
+                <Route path="/signin">
+                    <h1>Sign In</h1>
+                </Route>
+                <Route path="/browse">
+                    <h1>Browse</h1>
+                </Route>
+                <Route path="/">
+                    <h1>Home</h1>
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
