3168c9ff35ab984795518777a391f19c0f55c455
Artiom Kalugin <a.o.kalugin@protonmail.com>
2020-12-30 19:29:07 +0000

    Move routes into constants file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import Home from "./pages/home";
 
 const App = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/signup">
+                <Route path={ROUTES.SING_UP}>
                     <h1>Sign Up</h1>
                 </Route>
-                <Route path="/signin">
+                <Route path={ROUTES.SING_IN}>
                     <h1>Sign In</h1>
                 </Route>
-                <Route path="/browse">
+                <Route path={ROUTES.BROWSE}>
                     <h1>Browse</h1>
                 </Route>
-                <Route path="/">
+                <Route path={ROUTES.HOME}>
                     <Home/>
                 </Route>
             </Switch>
