38220799d48b076d6360bbf20c9e3748451e08d1
Artiom Kalugin <a.o.kalugin@protonmail.com>
2021-01-05 16:37:13 +0000

    Updated header for browse page.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import {Link as ReachRouterLink} from "react-router-dom";
-import {Background, Container, Logo, ButtonLink, Text, Link, Group} from "./styles/header";
+import { 
+    Background, 
+    Container, 
+    Logo, 
+    ButtonLink, 
+    Text, 
+    Link, 
+    Group, 
+    Feature, 
+    FeatureCallOut, 
+    PlayButton} from "./styles/header";
 
 const Header = ({bg=true, children, ...props}) => 
     bg ? <Background {...props}>{children}</Background> : children;
@@ -20,5 +30,11 @@ Header.Text = ({children, ...props}) => <Text {...props}>{children}</Text>;
 Header.Link = ({children, ...props}) => <Link {...props}>{children}</Link>;
 
 Header.ButtonLink = ({children, ...props}) => <ButtonLink {...props}>{children}</ButtonLink>
+
+Header.Feature = ({children, ...props}) => <Feature {...props}>{children}</Feature>;
+
+Header.FeatureCallOut = ({children, ...props}) => <FeatureCallOut {...props}>{children}</FeatureCallOut>;
+
+Header.PlayButton = ({children, ...props}) => <PlayButton {...props}>{children}</PlayButton>;
 
 export default Header;
