4c93aea302ab3f0e34571d744a077b4681d07458
Artiom Kalugin <a.o.kalugin@protonmail.com>
2021-01-06 08:52:06 +0000

    Create hook to retrieve data from firebase.

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import BrowseContainer from "../containers/browse";
+import {useContent} from "../hooks";
 
-const Browse = () => <BrowseContainer/>;
+const Browse = () => {
+    const {series} = useContent("series");
+    const {films} = useContent("films");
+
+    return <BrowseContainer/>;
+};
 
 export default Browse;
