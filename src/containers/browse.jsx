b6de88e721e4480579f59d6cba35ace2ba152289
Artiom Kalugin <a.o.kalugin@protonmail.com>
2021-01-06 11:25:53 +0000

    Implemented fuse search.

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -1,4 +1,5 @@
 import React, {useContext, useState, useEffect} from "react";
+import Fuse from "fuse.js";
 import {Header, Loading, Card} from "../components";
 import * as ROUTES from "../constants/routes";
 import {FirebaseContext} from "../context/firebase";
@@ -29,6 +30,17 @@ const BrowseContainer = ({slides}) => {
     useEffect(() => {
         setSlideRows(slides[category]);
     }, [slides, category]);
+
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+        
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+      }, [searchTerm])
 
     return profile.displayName ? (
         <>
