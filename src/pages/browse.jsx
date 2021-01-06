ce93d3fb523322c98fdbc1d8a5fb3d0f0d7647c0
Artiom Kalugin <a.o.kalugin@protonmail.com>
2021-01-06 09:00:25 +0000

    Added collection mapping utility.

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BrowseContainer from "../containers/browse";
 import {useContent} from "../hooks";
+import {selctionMap} from "../utils";
 
 const Browse = () => {
     const {series} = useContent("series");
     const {films} = useContent("films");
+    const slides = selectionMap({series, films});
 
     return <BrowseContainer/>;
 };
